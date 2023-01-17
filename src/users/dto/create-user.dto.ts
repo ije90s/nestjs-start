7187ad881b2784a75e1d7f5b0256b39b33f71eb2
ije90s <idgocm@gmail.com>
2023-01-17 18:04:57 +0900

    파이프

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,28 @@
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsString,
+  MaxLength,
+  MinLength,
+  Matches,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @Transform((params) => params.value.trim())
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  @Transform(({ value, obj }) => {
+    return value.trim();
+  })
+  @IsString()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
