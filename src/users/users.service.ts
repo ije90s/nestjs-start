99f10f92bdb005560073a1d76cf84a6d885b6063
ije90s <idgocm@gmail.com>
2023-01-18 16:59:42 +0900

    TypeORM

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,52 @@
 import * as uuid from 'uuid';
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { EmailService } from 'src/email/email.service';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserInfo } from './UserInfo';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
+import { ulid } from 'ulid';
+import { async } from 'rxjs';
 
 @Injectable()
 export class UsersService {
-  constructor(private emailService: EmailService) {}
+  constructor(
+    private emailService: EmailService,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+    private dadtaSource: DataSource,
+  ) {}
 
   async createUser(name: string, email: string, password: string) {
-    await this.checkUserExists(email);
+    const userExist = await this.checkUserExists(email);
+    if (userExist) {
+      throw new UnprocessableEntityException(
+        '해당 이메일로는 가입할 수 없습니다.',
+      );
+    }
 
     const signupVerifyToken = uuid.v1();
 
-    await this.saveUser(name, email, password, signupVerifyToken);
-    await this.sendMemberJoinEmail(email, signupVerifyToken);
+    const result = await this.saveUserUsingQueryRunner(
+      name,
+      email,
+      password,
+      signupVerifyToken,
+    );
+
+    // await this.saveUserUsingTransaction(
+    //   name,
+    //   email,
+    //   password,
+    //   signupVerifyToken,
+    // );
+
+    if (result) await this.sendMemberJoinEmail(email, signupVerifyToken);
   }
 
   async verifyEmail(signupVerifyToken: string): Promise<string> {
@@ -29,17 +61,79 @@ export class UsersService {
     throw new Error('Method not implemented');
   }
 
-  private checkUserExists(email: string) {
-    return false;
+  private async checkUserExists(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    return user !== null;
   }
 
-  private saveUser(
+  private async saveUser(
     name: string,
     email: string,
     password: string,
     signupVerifyToken: string,
   ) {
-    return;
+    const user = new UserEntity();
+    user.id = ulid();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.singupVerifyToken = signupVerifyToken;
+    await this.userRepository.save(user);
+  }
+
+  private async saveUserUsingQueryRunner(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ): Promise<boolean> {
+    let saveCheck = false;
+    const queryRunner = this.dadtaSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const user = new UserEntity();
+      user.id = ulid();
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.singupVerifyToken = signupVerifyToken;
+      await queryRunner.manager.save(user);
+
+      //throw new InternalServerErrorException();
+
+      await queryRunner.commitTransaction();
+
+      saveCheck = true;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+
+    return saveCheck;
+  }
+
+  private async saveUserUsingTransaction(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    await this.dadtaSource.transaction(async (manager) => {
+      const user = new UserEntity();
+      user.id = ulid();
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.singupVerifyToken = signupVerifyToken;
+
+      await manager.save(user);
+
+      //throw new InternalServerErrorException();
+    });
   }
 
   private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
